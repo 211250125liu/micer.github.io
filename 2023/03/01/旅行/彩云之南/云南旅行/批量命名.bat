1350028d11eaa743d541bd68208d0aa7cfddad67
211250125liu <211250125@smail.nju.edu.cn>
2023-03-02 20:52:21 +0800

    Site updated: 2023-03-02 20:52:16

---
+++ 2023/03/01/旅行/彩云之南/云南旅行/批量命名.bat
@@ -0,0 +1,282 @@
+@echo off
+Setlocal Enabledelayedexpansion
+set "str= "
+for /f "delims=" %%i in ('dir /b *.*') do (
+set "var=%%i" & ren "%%i" "!var:%str%=!")
+ren  tu(0).jpg  1.jpg
+ren  tu(1).jpg  2.jpg
+ren  tu(2).jpg  3.jpg
+ren  tu(3).jpg  4.jpg
+ren  tu(4).jpg  5.jpg
+ren  tu(5).jpg  6.jpg
+ren  tu(6).jpg  7.jpg
+ren  tu(7).jpg  8.jpg
+ren  tu(8).jpg  9.jpg
+ren  tu(9).jpg  10.jpg
+ren  tu(10).jpg  11.jpg
+ren  tu(11).jpg  12.jpg
+ren  tu(12).jpg  13.jpg
+ren  tu(13).jpg  14.jpg
+ren  tu(14).jpg  15.jpg
+ren  tu(15).jpg  16.jpg
+ren  tu(16).jpg  17.jpg
+ren  tu(17).jpg  18.jpg
+ren  tu(18).jpg  19.jpg
+ren  tu(19).jpg  20.jpg
+ren  tu(20).jpg  21.jpg
+ren  tu(21).jpg  22.jpg
+ren  tu(22).jpg  23.jpg
+ren  tu(23).jpg  24.jpg
+ren  tu(24).jpg  25.jpg
+ren  tu(25).jpg  26.jpg
+ren  tu(26).jpg  27.jpg
+ren  tu(27).jpg  28.jpg
+ren  tu(28).jpg  29.jpg
+ren  tu(29).jpg  30.jpg
+ren  tu(30).jpg  31.jpg
+ren  tu(31).jpg  32.jpg
+ren  tu(32).jpg  33.jpg
+ren  tu(33).jpg  34.jpg
+ren  tu(34).jpg  35.jpg
+ren  tu(35).jpg  36.jpg
+ren  tu(36).jpg  37.jpg
+ren  tu(37).jpg  38.jpg
+ren  tu(38).jpg  39.jpg
+ren  tu(39).jpg  40.jpg
+ren  tu(40).jpg  41.jpg
+ren  tu(41).jpg  42.jpg
+ren  tu(42).jpg  43.jpg
+ren  tu(43).jpg  44.jpg
+ren  tu(44).jpg  45.jpg
+ren  tu(45).jpg  46.jpg
+ren  tu(46).jpg  47.jpg
+ren  tu(47).jpg  48.jpg
+ren  tu(48).jpg  49.jpg
+ren  tu(49).jpg  50.jpg
+ren  tu(50).jpg  51.jpg
+ren  tu(51).jpg  52.jpg
+ren  tu(52).jpg  53.jpg
+ren  tu(53).jpg  54.jpg
+ren  tu(54).jpg  55.jpg
+ren  tu(55).jpg  56.jpg
+ren  tu(56).jpg  57.jpg
+ren  tu(57).jpg  58.jpg
+ren  tu(58).jpg  59.jpg
+ren  tu(59).jpg  60.jpg
+ren  tu(60).jpg  61.jpg
+ren  tu(61).jpg  62.jpg
+ren  tu(62).jpg  63.jpg
+ren  tu(63).jpg  64.jpg
+ren  tu(64).jpg  65.jpg
+ren  tu(65).jpg  66.jpg
+ren  tu(66).jpg  67.jpg
+ren  tu(67).jpg  68.jpg
+ren  tu(68).jpg  69.jpg
+ren  tu(69).jpg  70.jpg
+ren  tu(70).jpg  71.jpg
+ren  tu(71).jpg  72.jpg
+ren  tu(72).jpg  73.jpg
+ren  tu(73).jpg  74.jpg
+ren  tu(74).jpg  75.jpg
+ren  tu(75).jpg  76.jpg
+ren  tu(76).jpg  77.jpg
+ren  tu(77).jpg  78.jpg
+ren  tu(78).jpg  79.jpg
+ren  tu(79).jpg  80.jpg
+ren  tu(80).jpg  81.jpg
+ren  tu(81).jpg  82.jpg
+ren  tu(82).jpg  83.jpg
+ren  tu(83).jpg  84.jpg
+ren  tu(84).jpg  85.jpg
+ren  tu(85).jpg  86.jpg
+ren  tu(86).jpg  87.jpg
+ren  tu(87).jpg  88.jpg
+ren  tu(88).jpg  89.jpg
+ren  tu(89).jpg  90.jpg
+ren  tu(90).jpg  91.jpg
+ren  tu(91).jpg  92.jpg
+ren  tu(92).jpg  93.jpg
+ren  tu(93).jpg  94.jpg
+ren  tu(94).jpg  95.jpg
+ren  tu(95).jpg  96.jpg
+ren  tu(96).jpg  97.jpg
+ren  tu(97).jpg  98.jpg
+ren  tu(98).jpg  99.jpg
+ren  tu(99).jpg  100.jpg
+ren  tu(100).jpg  101.jpg
+ren  tu(101).jpg  102.jpg
+ren  tu(102).jpg  103.jpg
+ren  tu(103).jpg  104.jpg
+ren  tu(104).jpg  105.jpg
+ren  tu(105).jpg  106.jpg
+ren  tu(106).jpg  107.jpg
+ren  tu(107).jpg  108.jpg
+ren  tu(108).jpg  109.jpg
+ren  tu(109).jpg  110.jpg
+ren  tu(110).jpg  111.jpg
+ren  tu(111).jpg  112.jpg
+ren  tu(112).jpg  113.jpg
+ren  tu(113).jpg  114.jpg
+ren  tu(114).jpg  115.jpg
+ren  tu(115).jpg  116.jpg
+ren  tu(116).jpg  117.jpg
+ren  tu(117).jpg  118.jpg
+ren  tu(118).jpg  119.jpg
+ren  tu(119).jpg  120.jpg
+ren  tu(120).jpg  121.jpg
+ren  tu(121).jpg  122.jpg
+ren  tu(122).jpg  123.jpg
+ren  tu(123).jpg  124.jpg
+ren  tu(124).jpg  125.jpg
+ren  tu(125).jpg  126.jpg
+ren  tu(126).jpg  127.jpg
+ren  tu(127).jpg  128.jpg
+ren  tu(128).jpg  129.jpg
+ren  tu(129).jpg  130.jpg
+ren  tu(130).jpg  131.jpg
+ren  tu(131).jpg  132.jpg
+ren  tu(132).jpg  133.jpg
+ren  tu(133).jpg  134.jpg
+ren  tu(134).jpg  135.jpg
+ren  tu(135).jpg  136.jpg
+ren  tu(136).jpg  137.jpg
+ren  tu(137).jpg  138.jpg
+ren  tu(138).jpg  139.jpg
+ren  tu(139).jpg  140.jpg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
